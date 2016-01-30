8080f62bdcca0eb99c27299ca194d912b1162d81
Haunterck A <haunterck@linux-10jo.site>
2016-01-29 23:43:46 -0600

    Add basic use of celery with python

---
+++ celeryconfig.py
@@ -0,0 +1,16 @@
+BROKER_URL = 'amqp://'
+CELERY_RESULT_BACKEND = 'rpc://'
+
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT=['json']
+CELERY_TIMEZONE = 'America/Mexico_City'
+CELERY_ENABLE_UTC = True
+
+CELERY_ROUTES = {
+    'tasks.add': 'low-priority',
+}
+
+CELERY_ANNOTATIONS = {
+    'tasks.add': {'rate_limit': '10/m'}
+}
